eb8e9a06a8a0bf863d16d82fac8f5d9ee7c9c185
IgnacioGM1973 <I.GUIJARRO@HOTMAIL.ES>
2025-11-15 12:02:59 +0100

    Fifo2
    Segundo cambio

--- ProgramacionDeServicios_y_Procesos/AlgoritmosProcesos/AlgoritmosProcesos/src/FIFO/Main.java
+++ ProgramacionDeServicios_y_Procesos/AlgoritmosProcesos/AlgoritmosProcesos/src/FIFO/Main.java
@@ -1,12 +1,23 @@
 package FIFO;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scn = new Scanner(System.in);
+
+        int n;
+        System.out.println("Introduzca el número de procesos: ");
+        n = scn.nextInt();
 
 
 
-        for (int i = 1; i <= 5; i++) {
+
+
+
+
+        for (int i = 1; i <= n; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
