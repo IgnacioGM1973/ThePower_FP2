1ca81f54e413ba551272a0cba75f0f4e55adce6d
IgnacioGM1973 <I.GUIJARRO@HOTMAIL.ES>
2025-11-15 13:28:33 +0100

    ProgramaFinalizado

--- ProgramacionDeServicios_y_Procesos/AlgoritmosProcesos/AlgoritmosProcesos/src/FIFO/Main.java
+++ ProgramacionDeServicios_y_Procesos/AlgoritmosProcesos/AlgoritmosProcesos/src/FIFO/Main.java
@@ -8,6 +8,7 @@ public class Main {
         Scanner scn = new Scanner(System.in);
 
         int n;
+        int procesoMapeado = 1;
         System.out.println("Introduzca el número de procesos: ");
         n = scn.nextInt();  //Lee cuantos procesos vas a gestionar.
 
@@ -19,20 +20,51 @@ public class Main {
         int [] retorno = new int[n];// ráfaga  - llegada
         int [] espera = new int[n]; // tiempo que espera un proceso - tiempo de llegada
 
-        // Entrada de datos proceso.
+        // Entrada de datos proceso a proceso
 
+        for (int i = 0; i < n; i++) {
 
+            System.out.println("\nProceso " + (procesoMapeado++) + ": ");
+            System.out.println("Tiempo de llegada: ");
+            llegada[i] = scn.nextInt();
+            System.out.println("Tiempo de ráfaga: ");
+            rafaga[i] = scn.nextInt();
 
-
-
-
-
-
-
-        for (int i = 1; i <= n; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
         }
+        // Cálculo de tiempos de finalización
+        finalizacion[0] = llegada[0] + rafaga[0];
+
+        for (int i =1; i < n; i++){
+            if(llegada[i] > finalizacion[i-1]){
+                finalizacion[i] =llegada[i] + rafaga[i];
+
+            }else{
+                finalizacion[i] = finalizacion[i-1] + rafaga[i];
+            }
+        }
+        for (int i = 0;i < n;i++){
+            retorno[i] = finalizacion[i] - llegada[i];
+            espera[i] = retorno[i] - rafaga[i];
+        }
+        //Salida 
+        System.out.println("\n----------------------------------------------------");
+        System.out.println("Proceso\tLLegada\tRafaga\tFinal\tRetorno\tEspera");
+        System.out.println("\n----------------------------------------------------");
+        for (int i = 0; i < n; i++) {
+            System.out.println("P" +(i+1)+ "\t\t   "+llegada[i]+"\t  "+rafaga[i]+"\t\t"+finalizacion[i]+"\t\t"+retorno[i]+"\t\t"+espera[i]);
+
+        }
+        //Cálculo de promedio
+        double promRetorno = 0, promEspera = 0;
+        for (int i = 0; i < n; i++) {
+            promRetorno += retorno[i];
+            promEspera += espera[i];
+
+        }
+
+        System.out.printf("\nTiempo medio de retorno: %.2f", promRetorno / n);
+        System.out.printf("\nTiempo medio de espera: %.2f", promEspera / n);
+
+
     }
 }
